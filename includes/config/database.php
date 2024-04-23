d34e59addc05f0fae0ce962a1713b46aeb010983
LiaS1818 <sorgoroto@gmail.com>
2024-04-23 09:55:58 -0600

    archivo de base de datos2

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 <?php
 
 function conectarBD() : mysqli {
-    $db = new mysqli('10.0.0.5', 'root', '', 'appcbdsolutions'); // la forma orientada a objetos
+    $db = new mysqli('10.0.0.5', 'liaSan', '1234', 'registro'); // la forma orientada a objetos
 
     if (!$db) {
         echo "Error no se pudo conectar";
